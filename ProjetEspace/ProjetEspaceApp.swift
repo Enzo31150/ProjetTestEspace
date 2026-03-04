adb60bae9301b4e2a011913798aa1beaad3ae1c2
Bilal <bilal11@outlook.fr>
2026-03-04 15:07:47 +0100

    Ajout des fichiers xcode

---
+++ ProjetEspace/ProjetEspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetEspaceApp.swift
+//  ProjetEspace
+//
+//  Created by apprenant128 on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProjetEspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
