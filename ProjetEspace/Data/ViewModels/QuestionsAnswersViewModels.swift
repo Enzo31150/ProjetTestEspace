9cec27370049c9e68667004571fd75a366b61ad3
Leskeu <pro.theotherage@gmail.com>
2026-03-10 11:30:23 +0100

    ajout de models

--- ProjetEspace/Data/ViewModels/QuestionsAnswersViewModels.swift
+++ ProjetEspace/Data/ViewModels/QuestionsAnswersViewModels.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Observation
+
+@Observable
+class QuestionsAnswersViewModels {
+    
+    private let apikey: String =
+    "patcC8TZDmTftqNMb.21403494fe69345206fa582f8268d6da38a87e42ec2762b138ba4d37d57f0570"
+    private let baseURL = URL(
+        string:"https://api.airtable.com/v0/appHJzJ80jhZ6fY9f/questions?="
+    )!
+    var questions: [QuestionsAnswers] = []
+    
+    func fetchQuestionsAnwsers() async throws {
+        
+        var request = URLRequest(url: baseURL)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        do {
+            let decoded = try decoder.decode(QuestionsAnswersResponse.self, from: data)
+            
+            let questions = decoded.records.map { $0.fields}
+            self.questions = questions
+            
+        }catch{
+            print ("Echec du décodage : \(error)")
+            throw error
+        }
+    }
+}
