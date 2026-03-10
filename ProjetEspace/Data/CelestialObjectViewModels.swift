b23ad8c2d19b4dde5207fdc53e2a809883c2ce9a
Bilal <bilal11@outlook.fr>
2026-03-10 09:58:53 +0100

    ajout viewmodels celestialobjects & events

---
+++ ProjetEspace/Data/CelestialObjectViewModels.swift
@@ -0,0 +1,8 @@
+//
+//  CelestialObjectViewModels.swift
+//  ProjetEspace
+//
+//  Created by apprenant128 on 10/03/2026.
+//
+
+import Foundation
---
+++ ProjetEspace/Data/EventViewModels.swift
@@ -0,0 +1,8 @@
+//
+//  EventViewModels.swift
+//  ProjetEspace
+//
+//  Created by apprenant128 on 10/03/2026.
+//
+
+import Foundation
