3545be2aa6cb3e325bd2e3ae56b8b0240863601f
Akasueki <yanissery31@gmail.com>
2026-03-17 13:55:07 +0100

    Ajout de fichier quiz

---
+++ ProjetEspace/ScreenParts/QuizzCardView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizzCardView.swift
+//  ProjetEspace
+//
+//  Created by Apprenant 109 on 10/03/2026.
+//
+
+import SwiftUI
+
+struct QuizzCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizzCardView()
+}
---
+++ ProjetEspace/Screens/QuizChoiceView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizChoiceView.swift
+//  ProjetEspace
+//
+//  Created by Apprenant 109 on 16/03/2026.
+//
+
+import SwiftUI
+
+struct QuizChoiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizChoiceView()
+}
